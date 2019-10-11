b140ca3d91c4c54fdd2f093d8ea2b5bb23e13c7e
irbris <55032489+irbris@users.noreply.github.com>
2019-10-11 19:31:48 +0300

    feat: add task solution

--- src/fsm.js
+++ src/fsm.js
@@ -1,59 +1,79 @@
 class FSM {
-    /**
-     * Creates new FSM instance.
-     * @param config
-     */
-    constructor(config) {}
 
-    /**
-     * Returns active state.
-     * @returns {String}
-     */
-    getState() {}
+  constructor(config) {
+    if (!config) throw new Error("config is missing");
+    this.initialState = config.initial;
+    this.states = config.states;
+    this.state = config.initial;
+    this.history = [];
+    this.buffer = [];
+  }
 
-    /**
-     * Goes to specified state.
-     * @param state
-     */
-    changeState(state) {}
+  getState() {
+    return this.state;
+  }
 
-    /**
-     * Changes state according to event transition rules.
-     * @param event
-     */
-    trigger(event) {}
+  changeState(state) {
+    if (this.states[state]) {
+      this.history.push(this.state);
+      this.state = state;
+      this.buffer = [];
+    } else {
+      throw new Error("state isn\'t exist")
+    }
+  }
 
-    /**
-     * Resets FSM state to initial.
-     */
-    reset() {}
+  trigger(event) {
+    let newState = this.states[this.state].transitions[event];
+    if (newState) {
+      this.changeState(newState);
+    } else {
+      throw new Error("event in current state isn\'t exist");
+    }
+  }
 
-    /**
-     * Returns an array of states for which there are specified event transition rules.
-     * Returns all states if argument is undefined.
-     * @param event
-     * @returns {Array}
-     */
-    getStates(event) {}
+  reset() {
+    this.changeState(this.initialState);
+  }
 
-    /**
-     * Goes back to previous state.
-     * Returns false if undo is not available.
-     * @returns {Boolean}
-     */
-    undo() {}
+  getStates(event) {
+    if (!event) {
+      return Object.keys(this.states);
+    } else {
+      const result = [];
+      for (let key in this.states) {
+        if (this.states[key].transitions[event]) {
+          result.push(key);
+        }
+      } 
+      return result;
+    }
+  }
 
-    /**
-     * Goes redo to state.
-     * Returns false if redo is not available.
-     * @returns {Boolean}
-     */
-    redo() {}
+  undo() {
+    if (this.history.length) {
+      this.buffer.push(this.state);
+      this.state = this.history.pop();
+      return true;
+    } else {
+      return false;
+    }      
+  }
 
-    /**
-     * Clears transition history
-     */
-    clearHistory() {}
+  redo() {
+    if (this.buffer.length) {
+      this.history.push(this.state);
+      this.state = this.buffer.pop();
+      return true;
+    } else {
+      return false;
+    }    
+  }
+
+  clearHistory() {    
+    this.buffer = [];
+    this.history = [];
+  }
 }
 
 module.exports = FSM;
